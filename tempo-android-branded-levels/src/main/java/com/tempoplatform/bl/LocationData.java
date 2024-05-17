ed16b65770dbf2537a24fd850658c814a416115a
omnibaker <37693234+omnibaker@users.noreply.github.com>
2024-05-17 15:15:09 +1000

    reduce code

--- tempo-android-branded-levels/src/main/java/com/tempoplatform/bl/LocationData.java
+++ tempo-android-branded-levels/src/main/java/com/tempoplatform/bl/LocationData.java
@@ -1,7 +1,14 @@
 package com.tempoplatform.bl;
 
+// Consent
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+// Backups
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,56 +16,71 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class LocationData implements Serializable, Cloneable  {
-    public String consent;
-    public String state;
-    public String postcode;
-    public String country_code;
-    public String postal_code;
-    public String admin_area;
-    public String sub_admin_area;
-    public String locality;
-    public String sub_locality;
+    public String consent = "_consent";
+    public String state = "_state";
+    public String postcode = "_postcode";
+    public String country_code = "_country_code";
+    public String postal_code = "_postal_code";
+    public String admin_area = "_admin_area";
+    public String sub_admin_area = "_sub_admin_area";
+    public String locality = "_locality";
+    public String sub_locality = "_sub_locality";
 
-    // Always create as NONE (should be default anyway...)
     public LocationData() {
         consent = Constants.LocationConsent.NONE.toString();
     }
 
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    public static Constants.LocationConsent getLocationConsent(Context context) {
+
+        // PRECISE
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Utils.Warn("PRECISE: Precise location permission is granted, you can proceed with location-related tasks");
+            return Constants.LocationConsent.PRECISE;
+        }
+        // GENERAL
+        else if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Utils.Warn("GENERAL: General location permission is granted, you can proceed with location-related tasks");
+            return Constants.LocationConsent.GENERAL;
+        }
+        // NONE
+        else {
+            // Check if the user has denied the permission before
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                Utils.Warn("FALSE - User has denied the permission before, you can show a rationale explaining why you need the permission and request it again if the user agrees");
+            } else {
+                Utils.Warn("FALSE - User has not been asked for permission yet or has denied it with 'Never ask again' checked. You can prompt the user to grant the permission from the settings");
+            }
+            return Constants.LocationConsent.NONE;
+        }
     }
 
-    /**
-     *  Makes backup copy of current instance values - only to be implemented when a valid address has been identified
-     */
     public void backupAsJsonFile(Context context) {
 
         // Create a new File object for the JSON file
-        File directory = context.getDir(Constants.BACKUP_LOC_DIR, Context.MODE_PRIVATE);
-        String filename = Constants.BACKUP_LOC_FILE;
+        File directory = context.getDir(Constants.LOC_DIR, Context.MODE_PRIVATE);
+        String filename = Constants.LOC_FILE;
 
         // Do not continue is file/directory null or issue creating file
         if (directory != null && (filename != null && !filename.isEmpty())) {
             File file = new File(directory, filename);
             if (file != null) {
                 try {
-                    TempoUtils.Say("Backup location started: " + file.getAbsolutePath());
+                    Utils.Say("Backup location started: " + file.getAbsolutePath());
                     FileOutputStream fileOutputStream = new FileOutputStream(file);
                     ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
                     objectOutputStream.writeObject(this);
                     objectOutputStream.close();
                     fileOutputStream.close();
-                    TempoUtils.Say("Backup location saved: consent=" + consent + ", countryCode=" + country_code + ", adminArea==" + admin_area + ", subAdminArea==" + sub_admin_area + ", locality==" + locality);
+                    Utils.Say("Backup location saved: consent=" + consent + ", countryCode=" + country_code + ", adminArea==" + admin_area + ", subAdminArea==" + sub_admin_area + ", locality==" + locality);
                     return;
                 } catch (IOException e) {
-                    TempoUtils.Warn("Backup location failed: '" + e + "'");
+                    Utils.Warn("Backup location failed: '" + e + "'");
                     e.printStackTrace();
                     return;
                 }
             }
 
-            TempoUtils.Warn("Backup location failed - could not create file/dir");
+            Utils.Warn("Backup location failed - could not create file/dir");
         }
     }
 }
---
+++ tempo-android-branded-levels/src/main/java/com/tempoplatform/bl/Utils.java
@@ -0,0 +1,17 @@
+package com.tempoplatform.bl;
+
+import android.util.Log;
+
+public class Utils {
+
+    public static void Shout(String msg) {
+        Log.e(Constants.TEST_LOG, msg);
+    }
+    public static void Say(String msg) {
+        Log.d(Constants.TEST_LOG, msg);
+    }
+
+    public static void Warn(String msg) {
+        Log.d(Constants.TEST_LOG, msg);
+    }
+}
